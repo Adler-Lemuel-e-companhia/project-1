bdeb7a187d18e904cb2aba0a5d5ea7844c4b4222
daviadler06 <152089382+daviadler06@users.noreply.github.com>
2023-12-06 11:17:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,169 +1,119 @@
 import sqlite3
+from kivymd.uix.dialog import MDDialog
 from kivymd.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
 from kivy.lang import Builder
+from kivy.core.window import Window
 from kivymd.uix.snackbar import Snackbar
-from datetime import date
+from kivymd.uix.button import MDRaisedButton, MDFlatButton
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.label import MDLabel
 
-data = date.today()
 
 conn = sqlite3.connect('app.db')
 cursor = conn.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS dados(
-        data varchar(10),
-        preco REAL,
-        comb REAL,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome VARCHAR(80),
+        placa VARCHAR(10),
+        modelo VARCHAR(100) NULL,
+        lconsumo REAL,
         km REAL,
-        resultado REAL,
-        nome VARCHAR(50),
-        placa VARCHAR(10)
+        resultado REAL
     )
 ''')
 conn.commit()
 
-kv_string='''<Inicio>:
-    name:'inicio'
-    FloatLayout:
-        MDTopAppBar:
-            title: "[u][i]Tanque Cheio[/i][/u]"
-            pos_hint: {'center_x': 0.5,'center_y': 0.95}
-            elevation: 5
-            right_action_items: [['exit-to-app', lambda x: app.stop()]]
-            left_action_items: [['android']]
-
-        Image:
-            source:'logo.png'
-            size_hint: None, None
-            size: '200dp','200dp'
-            pos_hint:{'center_x': 0.5, 'center_y': 0.6}
-        MDRaisedButton:
-            text: 'Iniciar'
-            pos_hint: {'center_x': 0.5, 'center_y': 0.35}
-            on_release: root.manager.current = 'requisitos'
-<Requisitos>:
-    name:'requisitos'
-    MDTopAppBar:
-        title: "[u][i]Insira os dados[/i][/u]"
-        anchor_title: "left"
-        right_action_items: [['exit-to-app', lambda x: app.stop()]]
-        left_action_items: [["history", lambda x: root.mostrar_historico()]]
-        pos_hint: {'center_x': 0.5, 'center_y': 0.95}
-    MDTextField:
-        id: placa
-        multiline: False
-        size_hint_x: .8
-        pos_hint: {'center_x': 0.5,'center_y': 0.84}
-        hint_text: "Placa do veículo"
-        mode: "fill"
-    MDTextField:
-        id: comb
-        multiline: False
-        input_type: 'number'
-        size_hint_x: .8
-        pos_hint: {'center_x': 0.5,'center_y': 0.73}
-        hint_text: "Quantidade de reais em combustível"
-        mode: "fill"
-    MDTextField:
-        id: preco
-        multiline: False
-        input_type: 'number'
-        size_hint_x: .8
-        pos_hint: {'center_x': 0.5,'center_y': 0.62}
-        hint_text: "Preço do combustível"
-        mode: "fill"
-    MDTextField:
-        id: km
-        multiline: False
-        input_type: 'number'
-        size_hint_x: .8
-        pos_hint: {'center_x': 0.5,'center_y': 0.51}
-        hint_text: "Quilômetros rodados"
-        mode: "fill"
-    MDTextField:
-        id: nome
-        multiline: False
-        size_hint_x: .8
-        pos_hint: {'center_x': 0.5,'center_y': 0.40}
-        hint_text: "Nome do motorista"
-        mode: "fill"
-    MDFillRoundFlatButton:
-        pos_hint: {'center_x': 0.35,'center_y': 0.3}
-        text: "Calcular"
-        on_press: root.calcular()
-    MDFillRoundFlatButton:
-        pos_hint: {'center_x': 0.65,'center_y': 0.3}
-        text: "Limpar"
-        on_press: root.limpar()
-    MDLabel:
-        id: resposta
-        text: ''
-        font_size:
-        halign: 'center'
-        pos_hint: {'center_x': 0.5, 'center_y': 0.1}
-<Historico>:
-    name: 'historico'
-    MDTopAppBar:
-        title: "[u][i]Histórico[/i][/u]"
-        anchor_title: "left"
-        right_action_items: [['trash-can-outline', lambda x: root.apagar_historico()]]
-        left_action_items: [["keyboard-backspace", lambda x: root.voltar()]]
-        pos_hint: {'center_x': 0.5, 'center_y': 0.95}
-    MDLabel:
-        id: historico_label
-        text: ''
-        font_size: 20
-        halign: 'center'
-        pos_hint: {'center_x': 0.5, 'center_y': 0.5}'''
-
 class Historico(MDScreen):
-    def voltar(self):
+    def voltar_inicio(self):
         self.manager.current = 'requisitos'
-    def apagar_historico(self):
+    def apagar_confirmacao(self):
+        dialog = MDDialog(
+            title="Confirmação",
+            text="Tem certeza de que deseja apagar o histórico?",
+            buttons=[
+                MDRaisedButton(
+                    text="Cancelar", on_release=lambda *args: dialog.dismiss()
+                ),
+                MDRaisedButton(
+                    text="Apagar",
+                    on_release=lambda *args: self.apagar_historico(dialog),
+                ),
+            ],
+        )
+        dialog.open()
+    def apagar_historico(self, dialog):
         cursor.execute('DELETE FROM dados')
         conn.commit()
         self.ids.historico_label.text = "Histórico apagado"
-        self.show_temporary_message("Histórico apagado com sucesso")
-    def show_temporary_message(self, message):
-        self.snackbar = Snackbar(text=message)
+        self.temporario()
+        dialog.dismiss()
+    def temporario(self):
+        self.snackbar = Snackbar(text="Histórico apagado com sucesso")
         self.snackbar.open()
-
     def on_enter(self):
         cursor.execute('SELECT * FROM dados')
         data = cursor.fetchall()
         history_text = ""
         for entry in data:
-            history_text += f"ID: {entry[0]}, Preço: {entry[1]}, Combustível: {entry[2]}, KM: {entry[3]}, Resultado: {round(entry[4], 2)}, Nome: {entry[5]}, Placa: {entry[6]}\n"
+            history_text += f'''
+            ID: {entry[0]}
+            Nome: {entry[1]} 
+            Placa: {entry[2]}
+            Modelo: {entry[3]}
+            L/Consumidos: {entry[4]}
+            KM/Percorridos: {entry[5]}
+            Resultado: {round(entry[6], 2)}\n\n'''
         self.ids.historico_label.text = history_text
 class Inicio(MDScreen):
     pass
 class Requisitos(MDScreen):
+    def voltar_r(self):
+        self.manager.current = 'inicio'
+        self.manager.transition.direction = 'left'
     def mostrar_historico(self):
         self.manager.current = 'historico'
     def calcular(self):
-        preco = float(self.ids.preco.text)
-        comb = float(self.ids.comb.text)
-        km = float(self.ids.km.text)
-        nome = (self.ids.nome.text)
-        placa = (self.ids.placa.text)
-        
-        resultado = km / (comb / preco)
-        
-        cursor.execute('INSERT INTO dados(preco, comb, km, resultado, nome, placa) VALUES (?, ?, ?, ?, ?, ?)', (preco, comb, km, resultado, nome, placa))
-        conn.commit()
+            nome = (self.ids.nome.text)
+            placa = (self.ids.placa.text)
+            modelo = (self.ids.modelo.text)
+            try:
+                lconsumo = float(self.ids.lconsumo.text.replace(',', '.'))
+                km = float(self.ids.km.text.replace(',', '.'))
+            except ValueError:
+                self.ids.resposta.text = "Insira ou preencha valores válidos!"
+                self.ids.resposta.theme_text_color = "Custom"
+                self.ids.resposta.text_color = 1, 0, 0, 1
+                return
 
-        self.ids.resposta.text = f"A média total de gasto de combustível é: {resultado:.2f}"
+            resultado = km / lconsumo
+            cursor.execute('INSERT INTO dados(nome, placa, modelo, lconsumo, km, resultado) VALUES (?, ?, ?, ?, ?, ?)', (nome, placa, modelo, lconsumo, km, resultado))
+            conn.commit()
+            self.ids.resposta.text = f"A média de consumo de combustível é: {resultado:.2f}"
+            self.ids.resposta.theme_text_color = "Primary"
     def limpar(self):
-        self.ids.preco.text = ''
-        self.ids.comb.text = ''
-        self.ids.km.text = ''
-        self.ids.resposta.text = ''
         self.ids.nome.text = ''
         self.ids.placa.text = ''
+        self.ids.modelo.text = ''
+        self.ids.lconsumo.text = ''
+        self.ids.km.text = ''
+        self.ids.resposta.text = ''
+class Autonomia(MDScreen):
+    def voltar_r(self):
+        self.manager.current = 'inicio'
+        self.manager.transition.direction = 'left'
+class Viagem(MDScreen):
+    def voltar_r(self):
+        self.manager.current = 'inicio'
+        self.manager.transition.direction = 'left'
+
 class Main(MDApp):
+    dialog = None
     def build(self):
-        Builder.load_string(kv_string)
+        Window.size=(360, 640)
+        Builder.load_file('main.kv')
         self.theme_cls.theme_style="Dark"
         self.theme_cls.primary_palette = "Orange"
         self.title='Tanque cheio'
@@ -171,6 +121,21 @@ class Main(MDApp):
         sm.add_widget(Inicio())
         sm.add_widget(Requisitos())
         sm.add_widget(Historico())
+        sm.add_widget(Autonomia())
+        sm.add_widget(Viagem())
         return sm
-    
+    def aviso(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Sobre nós",
+                text="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", #EDITAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                auto_dismiss=True,
+                buttons=[
+                    MDFlatButton(
+                        text="Fechar",
+                        on_release= lambda *args: self.dialog.dismiss()
+                    ),
+                ],
+            )
+        self.dialog.open()
 Main().run()
